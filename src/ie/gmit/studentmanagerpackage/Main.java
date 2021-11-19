812102743769e4e3942f40402e746b1dc82321c3
dcregg <dcregg@GMITAZ-JMP73D3>
2021-11-19 10:37:02 +0000

    Deleted commented code and formatted

--- src/ie/gmit/studentmanagerpackage/Main.java
+++ src/ie/gmit/studentmanagerpackage/Main.java
@@ -212,7 +212,7 @@ public class Main extends Application {
 		if (noOfCmdLineArgs > 0) {
 			// Get command line arguments as String
 			cmdLineArgs = getParameters().getRaw().toString();
-			//System.out.println(cmdLineArgs);
+			// System.out.println(cmdLineArgs);
 			// Remove unwanted characters ([ and ] and ,)from string
 			cmdLineArgs = cmdLineArgs.replaceAll("\\[|\\]|\\,", "");
 			// System.out.println(cmdLineArgs);
@@ -232,48 +232,5 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
-
-//	// Main method containing print statement.
-//	public static void main(String[] args) {
-//
-////		// Create menu object
-////		Menu menuObject = new Menu();
-////		// Run menu start method
-////		menuObject.start();
-//		boolean addStatus;
-//
-//		// Add studnets from csv file
-//		StudentManager stuManObj = new StudentManager();
-////		File studentCSVFile = new File(".\\resources\\students.csv");
-//		File studentManagerByteFile = new File(".\\resources\\students.ser");
-////		stuManObj.loadStudentsFromCSVFile(studentCSVFile);
-//		stuManObj = stuManObj.loadStudentManagerObjectFromFile(studentManagerByteFile);
-//		System.out.println(stuManObj.findTotalStudents());	
-//		
-//		// Add one more student manually and write out all students to csv file
-//		Student studentObj1 = new Student("G00123461", "Jim", "Jones", 1);
-//		stuManObj.addStudent(studentObj1);
-//		System.out.println(stuManObj.findTotalStudents());
-////		stuManObj.saveStudentsToCSVFile(studentCSVFile);
-//		stuManObj.saveStudentManagerObjectToFile(studentManagerByteFile);
-//
-////		Student studentObject1 = new Student("G00123458", "Alf", "Stewart", 3);
-////		Student studentObject2 = new Student("G00123459", "Sally", "Fletcher", 4);
-////		Student studentObject3 = new Student("G00123459", "Sally", "Fletcher", 4);
-////
-////		addStatus = stuManObj.addStudent(studentObject1);
-////		System.out.println(addStatus);
-////		addStatus = stuManObj.addStudent(studentObject2);
-////		System.out.println(addStatus);
-////		addStatus = stuManObj.addStudent(studentObject3);
-////		System.out.println(addStatus);
-//
-//		// boolean removeStatus = stuManObj.removeStudent(studentObject1);
-//
-//		// System.out.println(removeStatus);
-//
-//		System.out.println("The End");
-//
-//	} // End main method
 
 } // End Main Class
--- src/ie/gmit/studentmanagerpackage/Student.java
+++ src/ie/gmit/studentmanagerpackage/Student.java
@@ -7,7 +7,7 @@ public class Student implements Serializable {
 
 	// Add for serialization
 	private static final long serialVersionUID = 1L;
-	
+
 	// Instance Variables
 	private String studentId;
 	private String firstName;
@@ -18,7 +18,7 @@ public class Student implements Serializable {
 	public Student(String studentId) {
 		this.studentId = studentId;
 	}
-	
+
 	// Constructor2
 	public Student(String studentId, String fisrtName, String surname) {
 		this.studentId = studentId;
@@ -66,36 +66,36 @@ public class Student implements Serializable {
 	public void setYearOfStudy(int yearOfStudy) {
 		this.yearOfStudy = yearOfStudy;
 	}
-	
+
 	public String findAllFieldValuesInCSVFormat() {
 		StringBuilder listOfFields = new StringBuilder();
-		//String newLine = System.getProperty("line.separator");
-		
+		// String newLine = System.getProperty("line.separator");
+
 		// determine fields declared in this class only (no fields of superclass)
 		Field[] fields = this.getClass().getDeclaredFields();
 
 		// print field names
 		for (Field field : fields) {
-		//	result.append("  ");
-		try {
-			// discard static fields
-			if (!java.lang.reflect.Modifier.isStatic(field.getModifiers())) {
-				//listOfFields.append(field.getName());
-				//requires access to private field:
-				listOfFields.append( field.get(this) );
-				listOfFields.append(",");
-		    }	
+			// result.append(" ");
+			try {
+				// discard static fields
+				if (!java.lang.reflect.Modifier.isStatic(field.getModifiers())) {
+					// listOfFields.append(field.getName());
+					// requires access to private field:
+					listOfFields.append(field.get(this));
+					listOfFields.append(",");
+				}
 				// requires access to private field:
-				//result.append(field.get(this));
+				// result.append(field.get(this));
 			} catch (IllegalAccessException ex) {
 				System.out.println(ex);
 			}
-			//result.append(newLine);
+			// result.append(newLine);
 		}
 		// Remove comma at end
-		if( listOfFields.length() > 0 ) {
-			listOfFields.setLength( listOfFields.length() - 1 );
-			//listOfFields.append(newLine); 
+		if (listOfFields.length() > 0) {
+			listOfFields.setLength(listOfFields.length() - 1);
+			// listOfFields.append(newLine);
 		}
 
 		return listOfFields.toString();
